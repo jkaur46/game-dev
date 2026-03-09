a29b5913321344351436369b07ec2b3d0451840f
jkaur46 <jkaur46@ftstudent.org>
2026-03-09 11:04:14 -0400

    a

--- coin.gd
+++ coin.gd
@@ -1,14 +1,9 @@
 extends Area2D
 
 
-<<<<<<< HEAD
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!")
-=======
 
 
 
 func _on_body_entered(body: Node2D) -> void:
 	print("+1 coin")
->>>>>>> 0ba0e0c2e11cca2d3aa5abd339c47fc10a1c2b55
 	queue_free()
